5b4283c8351b0bee8d2d16c6891f9ba29cac7928
kousukemorikawa-cell <kousuke.morikawa@hokkai.ed.jp>
2026-07-13 16:43:55 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const C='ptm-v15-1';
-const A=['./','./index.html','./styles-v15-1.css','./app-v15-1.js','./pokemon-data-v15-1.js','./moves-data-v15-1.js','./item-fairy-feather.png','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const C='ptm-v15-3';
+const A=['./','./index.html','./styles-v15-3.css','./app-v15-3.js','./pokemon-data-v15-3.js','./moves-data-v15-3.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k))))]))});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const x=r.clone();caches.open(C).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
