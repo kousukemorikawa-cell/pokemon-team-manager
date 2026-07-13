018092a646a02df220051a44e1c2a9e7f4f3844c
kousukemorikawa-cell <kousuke.morikawa@hokkai.ed.jp>
2026-07-13 15:04:00 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const C='ptm-v7';
-const A=['./','./index.html','./styles-v7.css','./app-v7.js','./pokemon-data-v7.js','./manifest.webmanifest'];
+const C='ptm-v8';
+const A=['./','./index.html','./styles-v8.css','./app-v8.js','./pokemon-data-v8.js','./moves-data-v8.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k))))]))});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const x=r.clone();caches.open(C).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
